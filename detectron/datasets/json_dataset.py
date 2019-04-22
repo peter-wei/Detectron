a960b1e88a300e26cf5901257a91aba7ec21639d
peter-wei <peterwei@umich.edu>
2019-04-22 00:42:44 -0400

    bs

--- detectron/datasets/json_dataset.py
+++ detectron/datasets/json_dataset.py
@@ -165,6 +165,7 @@ class JsonDataset(object):
         print(entry['id'])
         ann_ids = self.COCO.getAnnIds(imgIds=entry['id'], iscrowd=None)
         objs = self.COCO.loadAnns(ann_ids)
+        print(len(objs))
         # Sanitize bboxes -- some are invalid
         valid_objs = []
         valid_segms = []
